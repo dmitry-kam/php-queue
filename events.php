3b59d8cbe696b1e9f1fb15f97e66cdb61b6da2a6
DStarikov <DStarikov@elar.ru>
2020-10-31 12:50:11 +0300

    draft: daemon and events generator

---
+++ events.php
@@ -0,0 +1,77 @@
+<?php
+set_time_limit(0);
+$baseDir = dirname(__FILE__);
+ini_set('error_log',$baseDir.'/error.log');
+fclose(STDIN);
+fclose(STDOUT);
+fclose(STDERR);
+$STDIN = fopen('/dev/null', 'r');
+$STDOUT = fopen($baseDir.'/events.log', 'ab');
+$STDERR = fopen($baseDir.'/error.log', 'ab');
+
+pcntl_signal_dispatch();
+
+class EventsClass {
+    public $accountsQty = 1000;
+    public $eventsQty = 10000;
+    public $maxSendPerTime = 20;
+    protected $eventsQueue;
+
+    public function __construct() {
+        echo "Сonstructed events generator - ".date('d.m.Y H:i:s').PHP_EOL;
+        if (!file_exists(__DIR__.DIRECTORY_SEPARATOR.'/events')) {
+            mkdir(__DIR__.'/events', 0775);
+        }
+    }
+
+    public function generateQueue() {
+        $queue = range(1,$this->eventsQty);
+        $accountsQty = $this->accountsQty;
+
+        array_walk(
+            $queue,
+            function (&$v) use ($accountsQty) {
+                $v = rand(1, $accountsQty);
+            }
+        );
+//        while (count($queue)<$this->eventsQty) {
+//            shuffle($acc);
+//            $queue = array_merge($queue, $acc);
+//        }
+        $this->eventsQueue = $queue;
+        // $this->eventsQueue = array_reverse($queue);
+        echo "Generated ".count($this->eventsQueue)." events for ". $this->accountsQty. " accounts - ".date('d.m.Y H:i:s') . PHP_EOL;
+    }
+
+    public function run() {
+        echo "Running events generator - ".date('d.m.Y H:i:s').PHP_EOL;
+        $wereSended = 0;
+        $chunkIterator = 1;
+
+        $this->generateQueue();
+
+        while ($wereSended < $this->eventsQty) {
+            $fp = fopen(__DIR__.'/events/'.$chunkIterator.".json", "w");
+            fwrite($fp, json_encode((object)$chunkArr = array_slice($this->eventsQueue, $wereSended, rand(5, $this->maxSendPerTime), true)));
+//            fwrite($fp, json_encode($chunkAsString = implode(', ', array_map(
+//                function ($v, $k) { return $k."=>".$v; },
+//                $chunkArr = array_slice($this->eventsQueue, $wereSended, rand(5, $this->maxSendPerTime), true),
+//                array_keys($chunkArr)
+//            ))));
+            fclose($fp);
+            $wereSended += count($chunkArr);
+            //array_splice($this->eventsQueue, 0, -count($chunkArr));
+            $chunkIterator += 1;
+            echo "Chunk $chunkIterator (length = ".count($chunkArr).") was sended - ".date('d.m.Y H:i:s')." : ".implode(', ', array_map(
+                function ($v, $k) { return $k."=>".$v; },
+                $chunkArr,
+                array_keys($chunkArr)
+            )) . PHP_EOL;
+            //sleep(rand(1,3));
+            sleep(1);
+        }
+    }
+}
+
+$daemon = new EventsClass();
+$daemon->run();
